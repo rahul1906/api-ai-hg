53011bed50ac60b9fa1c20732f8fb84fb998267b
rahul1906 <rahulrmysore@gmail.com>
2017-08-17 18:30:59 +0530

    master

--- app.py
+++ app.py
@@ -33,9 +33,10 @@ def webhook():
 
     #print("Request:")
     #print(json.dumps(req, indent=4))
+    print(type(req))
 
     res = processRequest(req)
-
+    print(type(res))
     res = json.dumps(res, indent=4)
     # print(res)
     r = make_response(res)
@@ -51,33 +52,76 @@ def processRequest(req):
       baseurl = "https://pbh-uat.healthgrades.com/api/v4_0/providersearch/v4_0/pbh/search?cID=PBHTEST_007&providerType=None&what="+what+"&where="+where+"&sortBy=Score"
       data = get_data(baseurl)
       data = json.loads(data)
-      data = create_namelist(data)
+      # data = create_namelist(data)
+      # res = create_messages(data)
       res = create_messages(data)
       return res
 
+def create_messages(js):
+
+  if len(js['Results']) >= 5:
+    l1 = []
+    for i in range(0, 5):
+      a = {
+      "type" : 1,
+      "title" : js['Results'][i]['DemographicInfo']['DisplayName'],
+      "subtitle" : js['Results'][i]['DemographicInfo']['DisplayLastName'],
+      "imageUrl" : js['Results'][i]['DemographicInfo']['ImagePaths'][0]['Url'],
+      "buttons" : [{
+        "text" : "visit page" ,
+        "postback" : js['Results'][i]['DemographicInfo']['ProviderUrl']
+      }]
+      }
+      l1.append(a)
+      
+    print({"messages" : l1})
+    return {"messages" : l1}
+
+  else :  
+    for i in range(0,len(js['Results'])):
+      l1 = []
+      # l1 = [{'type':0,'speech':'these are the results that matched your search...'}]
+      # print(js['Results'][i]['DemographicInfo']['DisplayName'])
+      # print(js['Results'][i]['DemographicInfo']['ProviderUrl'])
+      # print(js['Results'][i]['DemographicInfo']['ImagePaths'][0]['Url'])
+      # print(js['Results'][i]['DemographicInfo']['DisplayLastName'])
+      a = {
+      "type" : 1,
+      "title" : js['Results'][i]['DemographicInfo']['DisplayName'],
+      "subtitle" : js['Results'][i]['DemographicInfo']['DisplayLastName'],
+      "imageUrl" : js['Results'][i]['DemographicInfo']['ImagePaths'][0]['Url'],
+      "buttons" : [{
+        "text" : "visit page" ,
+        "postback" : js['Results'][i]['DemographicInfo']['ProviderUrl']
+      }]
+      }
+      l1.append(a)
+      
+    print({"messages" : l1})  
+    return {"messages" : l1}
 
 def get_data(baseurl):
   headers = {
     "Accept": "application/json",
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjZCdFdLZ1g5RDd1ZGowYTJyLWkyZGFiN3dKRSIsImtpZCI6IjZCdFdLZ1g5RDd1ZGowYTJyLWkyZGFiN3dKRSJ9.eyJpc3MiOiJodHRwczovL3BiaC11YXQuaGVhbHRoZ3JhZGVzLmNvbS9hcGkvdjFfMC9zdHMvaWRlbnQiLCJhdWQiOiJodHRwczovL3BiaC11YXQuaGVhbHRoZ3JhZGVzLmNvbS9hcGkvdjFfMC9zdHMvaWRlbnQvcmVzb3VyY2VzIiwiZXhwIjoxNTAyOTczNDIyLCJuYmYiOjE1MDI5Njk4MjIsImNsaWVudF9pZCI6InBiaC1kZXZlbG9wZXJwb3J0YWwtc3dhZ2dlcmhhcm5lc3MtaW1wbGljaXQtY2xpZW50Iiwic2NvcGUiOiJwYmgucHJvdmlkZXJzZWFyY2gudjRfMCIsInN1YiI6IjgzYmVhOTQ1LWZmZGUtNGYzZC04YzU0LWUwZTBjYWJmNjZjMCIsImF1dGhfdGltZSI6MTUwMjY5MDE1OCwiaWRwIjoiaWRzcnYiLCJQcm92aWRlclNlYXJjaF92NF8wIjoiUGJoX1NlYXJjaF9HZXQiLCJhbXIiOlsicGFzc3dvcmQiXX0.PFBIL8lAn-1euaSdibIw7MQyG15IcNRi7-_myl4YgfnBy1UnYriZhfeUlWqLTe6VwhVSlm9gDs9brJ_nkNpXSfHFyPNz0joaCHCtgwD-EcyBgSuwZEqsHb1T9sh3apbntt73OpXI08iOfwc6Fr4y6atmR9kRIpyFssWU8UhLqZtr27SNwgGWFzeCoieQetDI4HqwcUR3fK069HmLVhNIIefCTwJa41IYrTL5P4tUqlkN3CFXHK7ei3UiiuwDjUJSdRN74MW6we9t0D35cYhTq2DWPsqLnG_qjGDmRciBMUz9YNJBBH_Hlj421tort69EK-CjrLkXZPbSOiliNAC9ig'
+    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjZCdFdLZ1g5RDd1ZGowYTJyLWkyZGFiN3dKRSIsImtpZCI6IjZCdFdLZ1g5RDd1ZGowYTJyLWkyZGFiN3dKRSJ9.eyJpc3MiOiJodHRwczovL3BiaC11YXQuaGVhbHRoZ3JhZGVzLmNvbS9hcGkvdjFfMC9zdHMvaWRlbnQiLCJhdWQiOiJodHRwczovL3BiaC11YXQuaGVhbHRoZ3JhZGVzLmNvbS9hcGkvdjFfMC9zdHMvaWRlbnQvcmVzb3VyY2VzIiwiZXhwIjoxNTAyOTc3NjY3LCJuYmYiOjE1MDI5NzQwNjcsImNsaWVudF9pZCI6InBiaC1kZXZlbG9wZXJwb3J0YWwtc3dhZ2dlcmhhcm5lc3MtaW1wbGljaXQtY2xpZW50Iiwic2NvcGUiOiJwYmgucHJvdmlkZXJzZWFyY2gudjRfMCIsInN1YiI6IjgzYmVhOTQ1LWZmZGUtNGYzZC04YzU0LWUwZTBjYWJmNjZjMCIsImF1dGhfdGltZSI6MTUwMjY5MDE1OCwiaWRwIjoiaWRzcnYiLCJQcm92aWRlclNlYXJjaF92NF8wIjoiUGJoX1NlYXJjaF9HZXQiLCJhbXIiOlsicGFzc3dvcmQiXX0.ZMSd4gEtR5tt7H-t4KpdnG5z1oP3Ffi_bnPOE5HklIMVVhdUlr2XBAeQHnv8UA6OIUYEEh7zVi3I7H7jjJ2GlgzdKzTyqD1Y6l5L8wJUDwKfZKd8wg504KTYHA8Z5_mxuE8XZbdpVfdM5AiwAsZjcFRy9qg8Y6-nT0Q3WlwKcuisKPDDE4GRpkkS14JcjJq-RVBeYWISZz4gdc0FPunklpNm6_UsYH3JDqdEmN8bYeXJLtGdL5I1iX0bR6W3mxUjqD7d9oo09MNL5FHBfq9En1k90KyYyFDKdWw-2MUA-FXh6-gu1uc-R24dqD_hvyJARr5uiewXremfK1361hML_A'
   }
   response = requests.get(baseurl, headers=headers)
   text = response.text
   return text
 
-def create_namelist(data):
-    l = []
-    for i in range(0, 5):
-        l.append(data['Results'][i]['DemographicInfo']['DisplayName'])
-    return l  
+# def create_namelist(data):
+#     l = []
+#     for i in range(0, 5):
+#         l.append(data['Results'][i]['DemographicInfo']['DisplayName'])
+#     return l  
 
-def create_messages(l):
-    l1 = [{'type':0,'speech':'these are the top 5 results that matched your search...'}]
-    for i in l:
-        a = {"type":0,
-        "speech":i}
-        l1.append(a)
-    return {"messages":l1}
+# def create_messages(l):
+#     l1 = [{'type':0,'speech':'these are the top 5 results that matched your search...'}]
+#     for i in l:
+#         a = {"type":0,
+#         "speech":i}
+#         l1.append(a)
+#     return {"messages":l1}
 
 # def processRequest(req):
 #     names = create_namelist(req)
